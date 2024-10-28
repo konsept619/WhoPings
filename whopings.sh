a405d15e77cf0768f1f6098c17082a5331f54217
konsept619 <konrad.wrzesien58@gmail.com>
2024-10-28 11:21:53 +0100

    Leaving simplified options (beter handling will be addedin future).

--- whopings.sh
+++ whopings.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-INTERFACE=$1
 LOG_FILE="/var/tmp/whopings"
 
 set -e 
 
 usage() {
-cat << EOF 
+  cat << EOF 
 This script must be used with sudo privileges
 Usage: $0 [interface] [options]
--b | --background   To be used in cron; output will be redirected to log file 
--i | --interactive  Output will be shown in terminal.
--h | --help         Displays help.
+-b    "background"  To be used in cron; output will be redirected to log file 
+-i    "interactive" Output will be shown in terminal.
+-h    "hepl"        Displays help.
 EOF
 }
 while getopts "b:i:h-:" flag; do 
   case "$flag" in
-    b|background)
+    b)
       echo "$0 is running in background. PID: $$ "
       tcpdump -i $OPTARG icmp and icmp[icmptype]=icmp-echo -n &>> $LOG_FILE &
       ;;
-    i|interactive) tcpdump -i $OPTARG icmp and icmp[icmptype]=icmp-echo -n
+    i) 
+      tcpdump -i $OPTARG icmp and icmp[icmptype]=icmp-echo -n
       ;;
-    h|help) usage
+    h)
+      usage
       exit 0
       ;;
-    \?) usage 
-      exit 1
-      ;;
+    \?)
+    echo "Invalid option: -$OPTARG"
+    exit 1
+    ;;
   esac
-
-
 done
